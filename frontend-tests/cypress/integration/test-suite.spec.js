75e55a05a2b815f9a1b6f2d83da68f8dd1c4284b
Matilda Björklund <matiildabjorklund@hotmail.com>
2020-10-30 21:46:38 +0100

    Created .gitignore and project

---
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+// This is a test suite
+describe('Test suite', function(){
+
+    // This is a test case
+    it('Create client', function(){
+        cy.visit('http://localhost:3000')
+        
+    })
+   
+    
+})
