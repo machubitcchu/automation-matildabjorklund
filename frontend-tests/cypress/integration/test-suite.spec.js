18d8069e92821f2e12b169c81cf02d52e41d9337
Matilda Björklund <matiildabjorklund@hotmail.com>
2020-10-31 18:48:00 +0100

    Installed mocha

--- frontend-tests/cypress/integration/test-suite.spec.js
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -6,7 +6,14 @@ describe('Test suite', function(){
     // This is a test case
     it('Create client', function(){
         cy.visit('http://localhost:3000')
-        
+        cy.title().should('include', "Hotel")
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+
     })
    
     
